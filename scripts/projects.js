0b4fd4184cadc1437ca0e33be61ff6aa7a416b7a
YC <yamile.colque@gmail.com>
2016-09-25 23:26:28 -0700

    minor changes

--- scripts/projects.js
+++ scripts/projects.js
@@ -4,9 +4,9 @@ function ProjectOb (input) {
   this.name = input.name;
   this.techtype = input.techtype;
   this.description = input.description;
-  //decide out how to include links to images (style)
 }
 
 ProjectOb.prototype.toHtml = function(){
-  //add to the DOM
+  var $newProject = $('section.template').clone();
+
 };
