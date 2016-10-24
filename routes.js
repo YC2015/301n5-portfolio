03cfd61781e08263ca3c3ea2711c21ceca12a169
YC <yamile.colque@gmail.com>
2016-10-23 20:41:41 -0700

    plan for class11

---
+++ routes.js
@@ -0,0 +1,9 @@
+
+//need to define callback functions
+//need to create controllers for each of the "pages" I am loading
+//need to insert the appropriate script tags into my html
+page('/', callback1);
+page('/Projects', callback2);
+page('/SociaMedia', callback3);
+
+page();
